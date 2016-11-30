0d000982d14287ce87895d9cd6b3506f2e73fbc9
webmobilidev <webmobilisviluppo@MacBook-Air-di-Luca.local>
2016-11-30 16:54:11 +0100

    prepare phone gap android
    solo ANDROID

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "phonegap-plugin-push.PushNotification",
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "phonegap-plugin-push": "1.8.4"
+};
+// BOTTOM OF METADATA
+});
